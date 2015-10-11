e4a589019f2aeb23552562261bcd47cff4fbf6ee
drnorthcutt <danielrnorthcutt@gmail.com>
2015-10-11 02:35:07 -0500

    feat:  Add tests for OMW, BYES, and uneven players.

--- vagrant/tournament/tournament_test.py
+++ vagrant/tournament/tournament_test.py
@@ -125,20 +125,100 @@ def testPairings():
     print "8. After one match, players with one win are paired."
 
 
-def testMultiPairings():
+def testExtras():
+    """Test extra credit options BYE and Swiss Pairings with BYE
+
+    If uneven players are registered, create BYE and pair randomly.  Use pair
+    to create first round, insert results and pair again.
+    """
+    # Clear data:
     deleteMatches()
     deletePlayers()
-    registerPlayer("D Vader")
-    registerPlayer("L Skywalker")
-    registerPlayer("P Leia")
-    registerPlayer("R2D2")
-    registerPlayer("J Hutt")
-    registerPlayer("Yoda")
-    registerPlayer("H Solo")
+    # Register players
+    players = ["D Vader", "L Skywalker", "P Leia", "R2D2", "J Hutt", "Yoda",
+               "H Solo"]
+    [registerPlayer(x) for x in players]
+    # Test BYE creation for uneven players
+    prior = countPlayers()
     pairings = swissPairings()
-    print "Test pairings 8 players"
-    print "Orig order: DVader,LSkywalker,PLeia,R2D2,JHutt,Yoda,HSolo"
-    print pairings
+    print "Original players:"
+    print prior
+    print ""
+    print "Current plays:"
+    after = countPlayers()
+    print after
+    if (after) != 8:
+        raise ValueError(
+            "BYE not created!")
+    if (after) == 8:
+        print ""
+    print "BYE created succcessfully."
+    print ""
+    # Test pairings
+    standings = playerStandings()
+    [id1, id2, id3, id4, id5, id6, id7, id8] = [row[0] for row in standings]
+    reportMatch(id1, id2)
+    reportMatch(id3, id4)
+    reportMatch(id5, id6)
+    reportMatch(id7, id8)
+    pairings = swissPairings()
+    if len(pairings) != 4:
+        raise ValueError(
+            "For 8 players, swissPairings should return 4 pairs.")
+    [(pid1, pname1, pid2, pname2), (pid3, pname3, pid4, pname4),
+    (pid5, pname5, pid6, pname6), (pid7, pname7, pid8, pname8)] = pairings
+    correct_pairs = set([frozenset([id1, id3]), frozenset([id5, id7])])
+    actual_pairs = set([frozenset([pid1, pid2]), frozenset([pid3, pid4])])
+    if correct_pairs != actual_pairs:
+        raise ValueError(
+            "After one match, players with one win should be paired.")
+    print "After one match, players with one win are paired."
+
+
+def test_omw():
+    """Test Opponent Match Wins
+
+    If players have same number of wins, OMW ranks players by opponent
+    wins. This test case checks for OMW proper ordering.
+    """
+    # Clear data:
+    deleteMatches()
+    deletePlayers()
+    # Register players:
+    players = ["Robert April", "Jonathan Archer", "Willard Decker",
+               "Rachel Garrett", "John Harriman", "Edward Jellico",
+               "James T. Kirk", "Jean-Luc Picard", "Christopher Pike",
+               "William T. Riker"]
+    [registerPlayer(x) for x in players]
+    standings = finalResults()
+    [id1, id2, id3, id4, id5, id6, id7, id8, id9, id10] = [row[0] for row in standings]
+    # Create matches:
+    reportMatch(id10, id9)
+    reportMatch(id1, id8)
+    reportMatch(id2, id7)
+    reportMatch(id3, id6)
+    reportMatch(id4, id5)
+    reportMatch(id10, id1)
+    reportMatch(id2, id3)
+    reportMatch(id4, id6)
+    reportMatch(id5, id7)
+    reportMatch(id8, id9)
+    reportMatch(id10, id2)
+    reportMatch(id1, id3)
+    reportMatch(id6, id8)
+    reportMatch(id6, id2)
+    # Check results
+    standings = finalResults()
+    print ""
+    print standings
+    print ""
+    correct_results = frozenset([id10, id4, id1, id6, id2, id5, id3, id8, id9, id7])
+    user_results = frozenset([row[0] for row in standings])
+    if correct_results != user_results:
+        print "Expected:", correct_results
+        print "Received:", user_results
+        raise ValueError("Incorrect player rank!")
+    print "OMW is implemented correctly"
 
 
 if __name__ == '__main__':
@@ -150,7 +230,12 @@ if __name__ == '__main__':
     testStandingsBeforeMatches()
     testReportMatches()
     testPairings()
+    print ""
+    print "Test Extra Credit"
+    print ""
     testMultiPairings()
+    test_omw()
+    print ""
     print "Success!  All tests pass!"
 
 
