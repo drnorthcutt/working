f34b07b28a348cc79d5312ddfb0f6b85c3972116
drnorthcutt <danielrnorthcutt@gmail.com>
2015-10-21 01:18:03 -0500

    recommit after system crash

---
+++ vagrant/restaurant/project.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, url_for, request, redirect
+app = Flask(__name__)
+
+from restaurant_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    return render_template('menu.html', restaurant=restaurant, items=items)
+
+@app.route('/restaurant/<int:restaurant_id>/<restaurant_name>/new/', methods=['GET','POST'])
+def newMenuItem(restaurant_id, restaurant_name):
+    if request.method == 'POST':
+        newItem = MenuItem(
+                            name=request.form['name'],
+                            price='$' +request.form['price'],
+                            description=request.form['desc'],
+                            restaurant_id=restaurant_id
+        )
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        rest = session.query(Restaurant).filter_by(id = restaurant_id).one()
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id, restaurant_name=restaurant_name)
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/', methods=['GET','POST'])
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a new menu item. Task 2 complete."
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/', methods=['GET','POST'])
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete."
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
--- vagrant/restaurant/server.py
+++ vagrant/restaurant/server.py
@@ -12,6 +12,49 @@ session = DBSession()
 class webserverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
+            if self.path.endswith("/edit"):
+                restaurantIDPath = self.path.split("/")[2]
+                query = session.query(Restaurant).filter_by(id = restaurantIDPath).one()
+                if query != []:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    output = "<html><body>"
+                    output += "<h1>"
+                    output += query.name
+                    output += "</h1>"
+                    output += ('''
+                                <form
+                                    method='POST'
+                                    enctype='multipart/form-data'
+                                    action='/restaurants/%s/edit'>
+                                ''') % restaurantIDPath
+                    output += "<input name='newRestaurantName' type='text placeholder='%s'>" % query.name
+                    output += "<input type='submit' value='Rename'>"
+                    output += "</form>"
+                    output += "</body></html>"
+                    self.wfile.write(output)
+
+            if self.path.endswith("/delete"):
+                restaurantIDPath = self.path.split("/")[2]
+                query = session.query(Restaurant).filter_by(id = restaurantIDPath).one()
+                if query != []:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    output = "<html><body>"
+                    output += "<h1>Are you sure you want to delete %s?</h1>" % query.name
+                    output += ('''
+                                <form
+                                    method='POST'
+                                    enctype='multipart/form-data'
+                                    action='/restaurants/%s/delete'>
+                                ''') % restaurantIDPath
+                    output += "<input type='submit' value='Delete'>"
+                    output += "</form>"
+                    output += "</body></html>"
+                    self.wfile.write(output)
+
             if self.path.endswith("/restaurants/new"):
                 self.send_response(200)
                 self.send_header('Content_type', 'text/html')
@@ -55,8 +98,9 @@ class webserverHandler(BaseHTTPRequestHandler):
                     output += "<p>"
                     output += row.name
                     output += "<br />"
-                    output += "<a href='#'>Edit</a> / "
-                    output += "<a href='#'>Delete</a>"
+                    output += "<a href='/restaurants/%s/edit'>Edit</a>" % row.id
+                    output += " / "
+                    output += "<a href='/restaurants/%s/delete'>Delete</a>" % row.id
                     output += "</p>"
                 output += "</body></html>"
                 self.wfile.write(output)
@@ -129,6 +173,34 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.send_header('Location', '/restaurants')
                 self.end_headers()
                 return
+
+            if self.path.endswith("/edit"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+                    restaurantIDPath = self.path.split("/")[2]
+                    myRestaurantQuery = session.query(Restaurant).filter_by(id=restaurantIDPath).one()
+                    if myRestaurantQuery != []:
+                        myRestaurantQuery.name = messagecontent[0]
+                        session.add(myRestaurantQuery)
+                        session.commit()
+                        self.send_response(301)
+                        self.send_header('Content-type', 'text/html')
+                        self.send_header('Location', '/restaurants')
+                        self.end_headers()
+
+            if self.path.endswith("/delete"):
+                restaurantIDPath = self.path.split("/")[2]
+                myRestaurantQuery = session.query(Restaurant).filter_by(id=restaurantIDPath).one()
+                if myRestaurantQuery != []:
+                    session.delete(myRestaurantQuery)
+                    session.commit()
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+
 #            self.send_response(301)
 #            self.end_headers()
 #            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
